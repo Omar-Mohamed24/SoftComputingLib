064d06613e64feb0cfacf7a8e3b78c4813057f15
Trickster <112867753+Omar-Mohamed24@users.noreply.github.com>
2025-10-15 16:54:46 +0300

    Refactor ReplacementStrategy interface definition

--- GA/interfaces/ReplacementStrategy.java
+++ GA/interfaces/ReplacementStrategy.java
@@ -6,6 +6,6 @@ import GA.chromosome.Chromosome;
  * Interface for replacement strategies.
  * Implementations define how to replace individuals in the population with offspring.
  */
-public interface ReplacementStrategy<T extends Chromosome> {
+public interface ReplacementStrategy {
     List<T> replace(List<T> currentPopulation, List<T> offspring, double[] fitnessValues, double[] offspringFitness);
 }
