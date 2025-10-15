9ee54382e2fc452f8a2431098612f976555eb7fd
Ahmed Saber <ahmedsaberahmed616@gmail.com>
2025-10-15 21:26:18 +0300

    (feat): Add Generational replacement strategy, (fix): Remove fitness value array parameter from replacement strategy interface.

--- GA/operator/replacement/GenerationalReplacement.java
+++ GA/operator/replacement/GenerationalReplacement.java
@@ -1,5 +1,13 @@
 package GA.operator.replacement;
 
-public class GenerationalReplacement {
-    
+import GA.chromosome.Chromosome;
+import GA.interfaces.ReplacementStrategy;
+
+import java.util.List;
+
+public class GenerationalReplacement implements ReplacementStrategy {
+    @Override
+    public List<Chromosome> replace(List<Chromosome> currentPopulation, List<Chromosome> offspring) {
+        return offspring;
+    }
 }
