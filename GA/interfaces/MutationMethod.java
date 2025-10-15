ca645089c7234ba166c8d5d9ead873d1c3901004
Trickster <112867753+Omar-Mohamed24@users.noreply.github.com>
2025-10-15 16:54:57 +0300

    Refactor MutationMethod interface definition

--- GA/interfaces/MutationMethod.java
+++ GA/interfaces/MutationMethod.java
@@ -5,6 +5,6 @@ import GA.chromosome.Chromosome;
  * Interface for mutation strategies.
  * Implementations define how to mutate chromosomes to introduce variation.
  */
-public interface MutationMethod<T extends Chromosome> {
+public interface MutationMethod {
     T mutate(T chromosome);
 }
