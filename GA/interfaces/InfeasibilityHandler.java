97c3aa9093a4fa19b78c3956c4c92bd9a9fec07a
Trickster <112867753+Omar-Mohamed24@users.noreply.github.com>
2025-10-15 16:55:12 +0300

    Refactor InfeasibilityHandler interface definition

--- GA/interfaces/InfeasibilityHandler.java
+++ GA/interfaces/InfeasibilityHandler.java
@@ -5,7 +5,7 @@ import GA.chromosome.Chromosome;
  * Interface to check for and handle invalid solutions.
  * Implementations should define how to detect and handle infeasible chromosomes.
  */
-public interface InfeasibilityHandler<T extends Chromosome> {
+public interface InfeasibilityHandler {
     boolean isFeasible(T chromosome);
     T repair(T chromosome);
 }
