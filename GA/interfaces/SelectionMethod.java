b1849be3857fede4e87a82d9fcaf95a811bb8097
Trickster <112867753+Omar-Mohamed24@users.noreply.github.com>
2025-10-15 16:54:29 +0300

    Refactor SelectionMethod interface for parent selection

--- GA/interfaces/SelectionMethod.java
+++ GA/interfaces/SelectionMethod.java
@@ -6,6 +6,6 @@ import GA.chromosome.Chromosome;
  * Interface for parent selection strategies.
  * Implementations define how parents are selected for reproduction.
  */
-public interface SelectionMethod<T extends Chromosome> {
-    List<T> select(List<T> population, double[] fitnessValues, int numberOfParents);
+public interface SelectionMethod {
+    List<T> select(List<T> population, int numberOfParents);
 }
